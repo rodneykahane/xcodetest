be57cf6ea5bf22eeb7d23239d90ab2d78def42fd
rodney kahane <rodney@rodney-kahanes-mac-mini.local>
2017-11-25 16:14:06 -0500

    Initial Commit

---
+++ stringTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  stringTest
+//
+//  Created by rodney kahane on 11/25/17.
+//  Copyright © 2017 rodney kahane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
